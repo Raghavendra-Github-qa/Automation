488c15fbec15134f62a18c5695a6d01a52f0a5b3
Raghavendra-Github-qa <43536509+Raghavendra-Github-qa@users.noreply.github.com>
2018-09-25 22:39:45 +0530

    Created BaseTest class under generic package

---
+++ src/generic/BaseTest.java
@@ -0,0 +1,23 @@
+package generic;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+	@BeforeMethod
+	public void openApp() {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("https://demo.actitime.com/login.do");
+	}
+	
+	@AfterMethod
+	public void closeApp(WebDriver driver) {
+		driver.close();
+	}
+
+}
